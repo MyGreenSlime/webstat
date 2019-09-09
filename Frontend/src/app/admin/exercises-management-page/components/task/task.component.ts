48f57319ea5167453719a949e5f0a423766a72ab
Chonlatas <chonlatas.t@gmail.com>
2019-09-09 19:16:05 +0700

    add addTask page

--- Frontend/src/app/admin/exercises-management-page/components/task/task.component.ts
+++ Frontend/src/app/admin/exercises-management-page/components/task/task.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  selector: "app-task",
+  templateUrl: "./task.component.html",
+  styleUrls: ["./task.component.scss"]
 })
 export class TaskComponent implements OnInit {
+  distributions = [
+    "Bernoulli",
+    "Bivariate normal",
+    "Binomial",
+    "Continuous Uniform",
+    "Discrete Uniform",
+    "Exponential",
+    "Geometric",
+    "Normal",
+    "Poisson"
+  ];
 
-  constructor() { }
+  distribution = "Bernoulli";
+
+  taskForm: any;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.taskForm = this.formBuilder.group({
+      title: ["", Validators.required],
+      name: ["", Validators.required],
+      distribution: ["Bernoulli", Validators.required],
+      disable: [false, Validators.required]
+    });
   }
-
 }
