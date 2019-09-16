776083f5b23a833ba301a07952e38e8ac9f66984
Chonlatas <chonlatas.t@gmail.com>
2019-09-16 22:48:30 +0700

    show result list

--- Frontend/src/app/home/home-page/home-page.component.ts
+++ Frontend/src/app/home/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { Router } from '@angular/router';
+import { GlobalService } from 'src/app/shared/services/global.service';
 
 @Component({
   selector: 'app-home-page',
@@ -10,14 +11,17 @@ import { Router } from '@angular/router';
 export class HomePageComponent implements OnInit {
 
   exercises: any;
+  user: any;
 
-  constructor(private apiService: ApiService, private router: Router) { }
+  constructor(private apiService: ApiService, private router: Router, private globalService: GlobalService) { }
 
   ngOnInit() {
     this.apiService.getExercisesAll().subscribe(res => {
       this.exercises = res.detail;
       console.log(this.exercises);
     })
+
+    this.user = this.globalService.getLocalStorage("user")
   }
 
 }
