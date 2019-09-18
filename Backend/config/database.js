dbf075db68bdfc79aab11642d3a095930b329435
don <ruksachai@gmail.com>
2019-09-19 01:01:38 +0700

    docker

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,5 +1,5 @@
-const MONGO_URL = process.env.MONGODB_PORT || "spire.cpe.ku.ac.th"
-const MONGO_PORT = process.env.MONGO_URL || "27017"
+const MONGO_URL = process.env.MONGO_URL || "spire.cpe.ku.ac.th"
+const MONGO_PORT = process.env.MONGODB_PORT || "27017"
 
 module.exports ={
     MONGO_PORT,MONGO_URL
