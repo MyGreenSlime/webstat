f102320240cb93a2f1763615696100b651fb00e8
Chonlatas <chonlatas.t@gmail.com>
2019-09-19 13:41:23 +0700

    fix logout

--- Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -25,6 +25,7 @@ export class NavbarComponent implements OnInit {
 
   logout() {
     this.authService.logout().subscribe(res => {
+      this.cookieService.deleteAll();
       this.router.navigate(['/login']);
     });
   }
--- Frontend/src/app/shared/helper/error.interceptor.ts
+++ Frontend/src/app/shared/helper/error.interceptor.ts
@@ -1,24 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { AuthService } from "../services/auth.service";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if ([401, 403].indexOf(err.status) !== -1) {
-                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                this.authService.logout();
-                this.router.navigate(['/login'])
-            }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError(err => {
+        if ([401, 403].indexOf(err.status) !== -1) {
+          // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+          this.cookieService.deleteAll();
+          this.router.navigate(["/login"]);
+        }
 
-            const error = err.error.message || err.statusText;
-            return throwError(error);
-        }))
-    }
+        const error = err.error.message || err.statusText;
+        return throwError(error);
+      })
+    );
+  }
 }
